08ff21c7e76652185c0317b2055ae19eaec52bec
Tom Ziegler <37410693+tfziegler@users.noreply.github.com>
2022-09-03 12:56:24 -0400

    Add files via upload

---
+++ approve manual agent.ps1
@@ -0,0 +1,17 @@
+﻿###################################################################
+#
+# Title: SCOM_Manual_Agent_Approval.ps1
+# Author: Tom Ziegler
+# Company: Microsoft
+# Last Modified: 3-4-2022
+# Version: 1.0
+#
+# Description - # Approves all Pending Management Except Name Like "MININT*"
+###################################################################
+
+$Session = New-PSSession -ComputerName OMMS1
+
+Import-Module -Name OperationsManager
+Invoke-Command -Session $Session -ScriptBlock{
+Get-SCOMPendingManagement | where {$_.AgentName -notlike "MININT*"} | Approve-SCOMPendingManagement
+}
